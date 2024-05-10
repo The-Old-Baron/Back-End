0766e94a644af28337393183536d5f7f97dd8401
Joao Vitor Ferreira <42494721+MestreSol@users.noreply.github.com>
2024-05-10 16:54:11 -0300

    🎨 refactor: Add IHistoryService and ITagService interfaces

---
+++ OldBarom.Core.Application/Mappings/DTOtoCommandMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using OldBarom.Core.Application.DTOs.Systempunk;
+using OldBarom.Core.Domain.Entities.Systempunk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OldBarom.Core.Application.Mappings
+{
+    public class DTOtoCommandMappingProfile : Profile
+    {
+        public DTOtoCommandMappingProfile()
+        {
+            CreateMap<HistoryDTO, History>();
+            CreateMap<TagDTO, Tag>();
+        }
+    }
+}
