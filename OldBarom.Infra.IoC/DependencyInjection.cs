cbbea08df17602a4fa161471e1bd2c74ea3a1573
Joao Vitor Ferreira <42494721+MestreSol@users.noreply.github.com>
2024-05-11 23:22:09 -0300

    Update test

---
+++ OldBarom.Infra.IoC/DependencyInjection.cs
@@ -0,0 +1,43 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OldBarom.Core.Application.Mappings;
+using OldBarom.Core.Domain.Account;
+using OldBarom.Core.Domain.Interfaces;
+using OldBarom.Infra.Data.Configure;
+using OldBarom.Infra.Data.Context;
+using OldBarom.Infra.Data.Identity;
+using OldBarom.Infra.Data.Repositories;
+
+namespace OldBarom.Infra.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration) {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.ConfigureApplicationCookie(options => options.AccessDeniedPath = "/Account/Login");
+
+            services.AddScoped<IAuthenticate, AuthenticateService>();
+            services.AddScoped<ISeedUserRoleInitial, UserConfigure>();
+
+            services.AddScoped<IHistoryRepository, HistoryRepository>();
+            services.AddScoped<ITagRepository, TagRepository>();
+
+            services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+            var myHandlers = AppDomain.CurrentDomain.Load("OldBarom.Core.Application");
+            //services.AddMediatR(myHandlers);
+
+            return services;
+        }
+    }
+}
---
+++ OldBarom.Infra.IoC/DependencyInjectionAPI.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OldBarom.Core.Application.Mappings;
+using OldBarom.Core.Domain.Account;
+using OldBarom.Core.Domain.Interfaces;
+using OldBarom.Infra.Data.Configure;
+using OldBarom.Infra.Data.Context;
+using OldBarom.Infra.Data.Identity;
+using OldBarom.Infra.Data.Repositories;
+
+namespace OldBarom.Infra.IoC
+{
+    public static class DependencyInjectionAPI
+    {
+        public static IServiceCollection AddInfrastructureAPI(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddScoped<IAuthenticate, AuthenticateService>();
+
+            services.AddScoped<IAuthenticate, AuthenticateService>();
+            services.AddScoped<ISeedUserRoleInitial, UserConfigure>();
+
+            services.AddScoped<IHistoryRepository, HistoryRepository>();
+            services.AddScoped<ITagRepository, TagRepository>();
+
+            services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+            var myHandlers = AppDomain.CurrentDomain.Load("OldBarom.Core.Application");
+            //services.AddMediatR(myHandlers);
+
+            return services;
+        }
+    }
+}
