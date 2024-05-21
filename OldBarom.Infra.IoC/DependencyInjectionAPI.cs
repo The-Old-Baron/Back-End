f773987edc48599a23fca6be80ed917494bade00
Joao Vitor Ferreira <42494721+MestreSol@users.noreply.github.com>
2024-05-21 17:29:48 -0300

    🚧 feat: Update Microsoft.AspNetCore.Identity.UI to version 8.0.5

---
+++ OldBarom.Infra.IoC/DependencyInjectionAPI.cs
@@ -0,0 +1,39 @@
+namespace OldBarom.Infra.IoC
+{
+    public class DependencyInjectionAPI
+    {
+        public static IServiceCollection AddInfrastructureAPI(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = "/Identity/Account/Login";
+                options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+            });
+
+            services.AddScoped<IAuthenticate, AuthenticateService>();
+            services.AddScoped<ISeedUserRoleInitial, SeedUserRoleInitial>();
+
+            services.AddScoped<IHistoryRepository, HistoryRepository>();
+            services.AddScoped<IHistoryService, HistoryService>();
+
+            services.AddScoped<ITagRepository, TagRepository>();
+            services.AddScoped<ITagService, TagService>();
+
+            services.AddScoped<IKeywordRepository, KeywordRepository>();   
+            services.AddScoped<IKeywordService, KeywordService>();
+            services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+            var myHandlers = AppDomain.CurrentDomain.Load("OldBarom.Core.Application");
+            services.AddMediatR(myHandlers);  
+
+            return services;
+        }
+    }
+}
